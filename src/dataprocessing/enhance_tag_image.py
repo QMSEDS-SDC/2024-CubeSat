7dd12cfb1bf4d647071cab0af159153db8f05364
Ayash Rath <ayashrath@gmail.com>
2024-12-10 07:46:26 +0000

    create enhance_tag_image.py

---
+++ src/dataprocessing/enhance_tag_image.py
@@ -0,0 +1,6 @@
+"""
+Consists of algorithms, that use image frames as inputs and enhance them to ensure better tag detection.
+The main final product has to be in greyscale, while the input needs to be in non-greyscale
+
+Supports: ArUco tags
+"""
