1835d5043c074201d4abeed0cc366e6fcc92bb16
Ayash Rath <ayashrath@gmail.com>
2024-12-23 05:40:06 +0000

    rename files, make misc file to test functiong of detect_card.py and add greyscale test

--- src/dataprocessing/detect_cards.py
+++ src/dataprocessing/detect_cards.py
@@ -1,3 +1,27 @@
 """
 Detects cards (rectangles) in an image and if detected then detects the number printed on it
 """
+
+import cv2
+import numpy as np
+from typing import Tuple, List, Dict
+
+
+def image_valid(img: np.ndarray) -> int:
+    """
+    Checks if an image is valid or not (valid array and greyscaled)
+
+    Parameter:
+        - img: The image array to check
+
+    Returns:
+        - 0: Image is valid
+        - -1: The image is not grey scale
+        - -2: The array is not valid for an image
+    """
+
+    if img.size == 0 or np.amin(img) < 0 or np.amax(img) > 255:
+        return -2
+    elif len(img.shape) != 2:
+        return -1
+    return 0
--- src/dataprocessing/image_preprocessing.py
+++ src/dataprocessing/image_preprocessing.py
@@ -2,5 +2,5 @@
 Consists of algorithms, that use image frames as inputs and enhance them to ensure better tag detection.
 The main final product has to be in greyscale, while the input needs to be in non-greyscale
 
-Supports: ArUco tags
+Supports: ArUco tags and Number Cards
 """
