5cdb424cb8f403ec2daef18f8a2b7f3cca8bd20a
nrahman57 <nrahman7999@gmail.com>
2025-06-28 15:36:44 +0100

    Update RBYPIStatus.py

--- src/API/comms/RBYPIStatus.py
+++ src/API/comms/RBYPIStatus.py
@@ -3,11 +3,13 @@
 import subprocess
 from pyembedded.raspberry_pi_tools.raspberrypi import PI
 from Camera import Camera
+from APILogs import apilogs  
 
 class status:
     # Backend Code. Relavant functions for the GUI are below
     def __init__(self):
         # Initialising
+        self.logs = apilogs()  
         self.pi = PI()
         self.camera = Camera()
         self.command = ["iw", "dev", "wlan0", "link"]
@@ -49,33 +51,55 @@ class status:
 
     # returns temp
     def pitemp(self):
-        return self.pidetect(self.pi.get_cpu_temp) # returns cpu temperature
+        x = self.pidetect(self.pi.get_cpu_temp)
+        self.logs.addlog("CPU Temperature Request --- Value: " + str(x))
+        return x # returns cpu temperature
     # returns cpu usage
     def piuse(self):
-        return self.pidetect(self.pi.get_cpu_usage) # returns cpu usage as a %
+        x = self.pidetect(self.pi.get_cpu_usage)
+        self.logs.addlog("CPU Usage Request --- Value: " + str(x))
+        return x # returns cpu usage as a %
     # returns disk space
     def pidisk(self):
-        return self.pidetecta(self.pi.get_disk_space, 3) # returns disk usage
+        x = self.pidetecta(self.pi.get_disk_space, 3)
+        self.logs.addlog("Disk Usage Request --- Value: " + str(x))
+        return x # returns disk usage
     # returns ram
     def piusedram(self):
-        return self.pidetecta(self.pi.get_ram_info, 1) # returns Used RAM
+        x = self.pidetecta(self.pi.get_ram_info, 1)
+        self.logs.addlog("Used RAM Request --- Value: " + str(x))
+        return x # returns Used RAM
     def pifreeram(self):
-        return self.pidetecta(self.pi.get_ram_info, 2) # returns Free Ram
+        x = self.pidetecta(self.pi.get_ram_info, 2)
+        self.logs.addlog("Free RAM Request --- Value: " + str(x))
+        return x # returns Free RAM
     # returns wifi status
     def piwifiquality(self):
-        return self.pidetecta(self.pi.get_wifi_status, 1) # returns wifi quality
+        x = self.pidetecta(self.pi.get_wifi_status, 1)
+        self.logs.addlog("WiFi Quality Request --- Value: " + str(x))
+        return x # returns wifi quality
     # returns wifi strength
     def piwifistrength(self):
-        return self.parseinfo(self.runcommand(self.command), 0) # Strength in dBm
+        x = self.parseinfo(self.runcommand(self.command), 0)
+        self.logs.addlog("WiFi Strength Request --- Value: " + str(x))
+        return x # Strength in dBm
     # returns wifi frequency
     def piwififreq(self):
-        return self.parseinfo(self.runcommand(self.command), 1) # returns wifi frequency in MHz
+        x = self.parseinfo(self.runcommand(self.command), 1)
+        self.logs.addlog("WiFi Frequency Request --- Value: " + str(x))
+        return x # returns wifi frequency in MHz
     # returns wifi tx bitrate
     def piwifitxbitrate(self):
-        return self.parseinfo(self.runcommand(self.command), 2) # returns wifi tx bitrate in Mbps
+        x = self.parseinfo(self.runcommand(self.command), 2)
+        self.logs.addlog("WiFi TX Bitrate Request --- Value: " + str(x))
+        return x # returns wifi tx bitrate in Mbps
     # returns ip address
     def piip(self):
-         return self.pidetect(self.pi.get_connected_ip_addr(network='wlan0')) # returns the ip address of the connected network
+        x = self.pidetect(self.pi.get_connected_ip_addr(network='wlan0'))
+        self.logs.addlog("IP Address Request --- Value: " + str(x))
+        return x # returns the ip address of the connected network
     # returns camera status
     def GetCameraStatus(self):
-        return self.pidetect(self.camera.GetStatus())
+        x = self.pidetect(self.camera.GetStatus)
+        self.logs.addlog("Camera Status Request --- Value: " + str(x))
+        return x # returns camera status
